a891a6b2bbf1f4a897ccdd98178799571f8206a2
juan cantero <juan.cantero@outlook.com>
2022-08-29 21:02:53 -0300

    finish login reactive

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength.validator";
 
+let { required, email, minLength } = Validators;
 
 @Component({
-  selector: 'login',
-  templateUrl: './login-reactive.component.html',
-  styleUrls: ['./login-reactive.component.css']
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
+  form = this.fb.group({
+    email: [
+      "",
+      {
+        validators: [required, email],
+      },
+    ],
+    password: ["", [required, minLength(8), createPasswordStrengthValidator()]],
+  });
 
+  constructor(private fb: FormBuilder) {}
 
-  constructor() {
+  ngOnInit() {}
 
-
+  showError(control: FormControl, error: string) {
+    return control.dirty && control.errors[error];
   }
-
-  ngOnInit() {
-
-  }
-
 }
